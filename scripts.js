6f593aad2afa2f2f21959c79125e8542891bc366
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-09-29 23:55:41 -0700

    added placeholders and started working on color display

--- scripts.js
+++ scripts.js
@@ -8,16 +8,27 @@ const heightDisplay = document.getElementById('heightDisplay')
 const colorInput = document.getElementById('colorInput')
 const colorDisplay = document.getElementById('colorDisplay')
 
-const colorBlob = document.getElementById('colorBlob')
+const blob = document.getElementById('colorBlob')
+
+// placeholders for page refresh
+widthDisplay.innerHTML = 100
+heightDisplay.innerHTML = 100
+colorDisplay.innerHTML = "#000000"
+
+// color blob on bottom right
+// blob.style.width = `${widthInput.value}`
+// blob.style.height = `${heightInput.value}`
+// blob.style.backgroundColor = `${colorInput.value}`
 
 // event listeners
 widthInput.addEventListener('input', update)
-heightInput.addEventListener('input', update);
-colorInput.addEventListener('input', update);
+heightInput.addEventListener('input', update)
+colorInput.addEventListener('input', update)
 
 // update to inputs
 function update(e) {
     widthDisplay.innerHTML = `${widthInput.value}`
     heightDisplay.innerHTML = `${heightInput.value}`
     colorDisplay.innerHTML = `${colorInput.value}`
+    // colorBlob.innerHTML = `${colorInput.value}`
 }
